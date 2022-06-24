662e02e6ea385eed59e8ce9943c7121c88742c78
nahomjim91 <nahomjim91@gmail.com>
2022-06-24 18:50:00 +0000

    Assignment 1 hello worrld

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+/*
+	author: Selihom kidane
+	Sec: DBRSE2102 A
+*/
+namespace Ass
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("hello world ")
+		}
+	}
+}
